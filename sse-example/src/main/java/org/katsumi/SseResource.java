acacea4c1c87fd23a189a063e36a89a3fe4ef622
Katsumi Kokuzawa <katsumi.kokuzawa@me.com>
2015-12-20 12:44:02 +0900

    実際の利用シーンに合わせたサンプルになるように修正

--- sse-example/src/main/java/org/katsumi/SseResource.java
+++ sse-example/src/main/java/org/katsumi/SseResource.java
@@ -4,39 +4,40 @@ import org.glassfish.jersey.media.sse.EventOutput;
 import org.glassfish.jersey.media.sse.OutboundEvent;
 import org.glassfish.jersey.media.sse.SseFeature;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
 
-@Path("events")
+@Path("sse")
+@Singleton
 public class SseResource
 {
+    private List<EventOutput> eventOutputs = new ArrayList<>();
+
     @GET
+    @Path("events")
     @Produces(SseFeature.SERVER_SENT_EVENTS)
     public EventOutput getServerSentEvents()
     {
         final EventOutput eventOutput = new EventOutput();
-        new Thread(() -> {
-            try {
-                try {
-                    for (int i = 0; i < 10; i++) {
-                        TimeUnit.SECONDS.sleep(1);
-                        final OutboundEvent.Builder builder = new OutboundEvent.Builder();
-                        builder.name("message-to-client");
-                        builder.data(String.class, "Hello world " + i + "!");
-                        eventOutput.write(builder.build());
-                    }
-                }
-                finally {
-                    eventOutput.close();
-                }
-            }
-            catch (InterruptedException | IOException e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
+        eventOutputs.add(eventOutput);
         return eventOutput;
+    }
+
+    @PUT
+    @Path("put")
+    public void putData() throws IOException
+    {
+        for (EventOutput eventOutput : eventOutputs) {
+            final OutboundEvent.Builder builder = new OutboundEvent.Builder();
+            builder.name("message-to-client");
+            builder.data(String.class, "登録された！");
+            eventOutput.write(builder.build());
+        }
     }
 }
